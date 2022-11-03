2c834360ebab97f5090b10969fd8070cc186e7ae
Fenickss <denis.suxov.1991@mail.ru>
2022-11-03 13:22:39 +0300

    +

--- dist/index.be164b19.js
+++
@@ -1,4 +0,0 @@
-const url = `https://restcountries.eu/rest/v2`;
-fetch(url).then((response)=>console.log(response.json())); // function fetchCountryByName(name) { }
-
-//# sourceMappingURL=index.be164b19.js.map
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import APP from "./js/fetchCountries";
 import "./js/fetchCountries";
+
+import featchCount from "./js/fetchCountries";
+
+let refs = {
+  input: document.querySelector(".js-countrySearch"),
+  out: document.querySelector(".outSearch"),
+};
+
+refs.input.addEventListener("input", onInputChange);
+
+// Достучаться до input value и вывести в out
+function onInputChange(event) {
+  console.dir(event.currentTarget.value);
+}

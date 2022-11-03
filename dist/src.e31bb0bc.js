ab3d06ce130a40874b49fb08f949b1b6951cb7c9
Fenickss <denis.suxov.1991@mail.ru>
2022-11-03 15:04:19 +0300

    +

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -547,7 +547,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "27381" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34011" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];

25f27691a367ef85dc68d3025f99b582f6d09458
Fenickss <denis.suxov.1991@mail.ru>
2022-11-08 10:36:53 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import "./js/fetchCountries";
+import API from "./js/fetchCountries";
 import _debounce from "lodash.debounce";
-import featchCount from "./js/fetchCountries";
-
+console.log(API);
 let refs = {
   input: document.querySelector(".js-countrySearch"),
   out: document.querySelector(".outSearch"),
 };
 
-const debounceOnInput = _debounce(onInputChange, 1000);
+const debounceOnInput = _debounce(onInputChange, 500);
 refs.input.addEventListener("input", debounceOnInput);
 
 // Достучаться до input value и вывести в out
 function onInputChange(event) {
   refs.out.textContent = event.target.value;
 }
+
+// Подключаемся к промис
+function renderCountryCard(country) {
+  const markup = countryCardTpl(country);
+  cardContainer.innerHTML = markup;
+}
+console.log(renderCountryCard());
+
+console.log(API);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,13 @@
+import markupTeamplateCard from "../teamplate/teamplate-card.hbs";
+
 fetch(`https://restcountries.com/v2/all`)
   .then((response) => {
     return response.json();
   })
   .then((name) => {
-    console.log(name);
+    // console.log(name);
+    const markup = markupTeamplateCard(name);
+    // console.log(markup);
   })
   .catch((error) => {
     console.log(error);
